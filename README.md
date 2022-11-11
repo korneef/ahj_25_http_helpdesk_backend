5859000f5a5c2414f736eb7cbe4256600e16e1c6
Valentin Korneev <v.korneef@gmail.com>
2022-11-11 16:46:17 +0600

    homework v1.0

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-[![Build Status](https://api.cirrus-ci.com/github/korneef/ahj_25_http_helpdesk_backend.svg)](https://cirrus-ci.com/github/korneef/ahj_25_http_helpdesk_backend)
-
-https://korneef.github.io/ahj_25_http_helpdesk_backend
+It's a backend
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(koaBody({
 const tickets = [
   {
     "name": "ticket1",
-    "status": false,
+    "status": true,
     "date": 1667911504252,
     "id": 60014
   }
@@ -20,7 +20,7 @@ const tickets = [
 const ticketsFull = [
   {
     "name": "ticket1",
-    "status": false,
+    "status": true,
     "date": 1667911504252,
     "description": 'this is ticket1 description',
     "id": 60014
@@ -61,7 +61,6 @@ app.use(async ctx => {
 
   const method = ctx.request.method;
   let process = null;
-
   switch (method) {
     case 'GET':
       process = ctx.request.query.process;
@@ -76,10 +75,10 @@ app.use(async ctx => {
 
   switch (process) {
     case 'allTickets':
-      ctx.response.body = tickets;
+      ctx.response.body = JSON.stringify(tickets);
       return;
     case 'ticketById':
-      const id = ctx.request.query.id
+      const id = ctx.request.query.id;
       if (!isNaN(Number(id))) {
         let ticket = ticketsFull.find((item) => item.id === Number(id));
         if (ticket) {
@@ -98,13 +97,59 @@ app.use(async ctx => {
       const ticketFull = new TicketFull(ticketName, ticketDescription)
       ticketFull.id = ticketId;
       ticketsFull.push(ticketFull);
-
       ctx.response.status = 200;
+      ctx.response.body = ticket;
       return
+    case 'editTicket':
+      const editedId = ctx.request.body.id;
+      if (!isNaN(Number(editedId))) {
+        let ticket = tickets.find((item) => item.id === Number(editedId));
+        let ticketFull = ticketsFull.find((item) => item.id === Number(editedId));
+        if (ticket !== undefined && ticketFull !== undefined) {
+          ticket.name = ctx.request.body.name;
+          ticketFull.name = ctx.request.body.name;
+          ticket.description = ctx.request.body.description;
+          ticketFull.description = ctx.request.body.description;
+          ctx.response.status = 200;
+          ctx.response.body = ticketFull;
+          return
+        }
+      }
+    case 'deleteTicket':
+      const ticketDeleteId = ctx.request.body.id;
+      const ticketIndex = tickets.findIndex((item) => Number(item.id) === Number(ticketDeleteId));
+      const ticketFullIndex = ticketsFull.findIndex((item) => Number(item.id) === Number(ticketDeleteId));
+      if (ticketIndex !== -1 && ticketFullIndex !== -1) {
+        tickets.splice(ticketIndex, 1);
+        ticketsFull.splice(ticketFullIndex, 1);
+        ctx.response.status = 200;
+        ctx.response.body = ticketDeleteId;
+        return
+      } else {
+        ctx.response.status = 404;
+        return
+      }
+      case 'checkboxChange':
+        const ticketCheckboxId = ctx.request.body.id;
+        if (!isNaN(Number(ticketCheckboxId))) {
+          let ticket = tickets.find((item) => item.id === Number(ticketCheckboxId));
+          let ticketFull = ticketsFull.find((item) => item.id === Number(ticketCheckboxId));
+          if (ticket !== undefined && ticketFull !== undefined) {
+            if (ctx.request.body.status === 'true') {
+              ticket.status = true;
+              ticketFull.status = true;
+            } else {
+              ticket.status = false;
+              ticketFull.status = false; 
+            }
+            ctx.response.status = 200;
+            return
+          }
+        }
   }
 
   ctx.response.status = 404;
   return
 });
-
-const server = http.createServer(app.callback()).listen(7070);
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback()).listen(port);
